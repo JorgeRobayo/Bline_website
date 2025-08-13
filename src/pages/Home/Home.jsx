29cb3b027d3d2dc8d9c56028569023b46b436d03
jorge robayo <jorgerobmor@gmail.com>
2025-08-12 20:41:15 -0400

    title cards adedd to the home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Home.css";
 import Navbar from "../../components/Navbar/Navbar";
+import TitleCards from "../../components/TitleCards/TitleCards";
 import hero_banner from "../../assets/hero_banner.jpg";
 import hero_title from "../../assets/hero_title.png";
 import play_icon from "../../assets/play_icon.png";
@@ -28,7 +29,8 @@ const Home = () => {
               <img src={info_icon} alt="" />
               More Info
             </button>
-          </div>
+            
+          </div><TitleCards/>
         </div>
       </div>
     </div>
